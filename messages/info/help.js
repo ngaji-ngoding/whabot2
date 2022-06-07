1018501ccecea7c3aa964d5b9105b69a886e5810
ngaji-ngoding <mahsun.b321@gmail.com>
2022-06-07 11:28:05 +0700

    create message template

---
+++ messages/info/help.js
@@ -0,0 +1,8 @@
+module.exports = function (nama) {
+  return {
+    text:
+      "hallo " +
+      nama +
+      "!!! selamat datang di bot *NGAJI NGODING*\n\nfitur yang tersedia adalah sebagai berikut\n\n*Info*\n*Test*\n\noption yang tersedia untuk fitur *Info* adalah\n*help*\n*user*\n\nselamat mencoba fitur fitur bot ini!!!\nterima kasih",
+  };
+};
